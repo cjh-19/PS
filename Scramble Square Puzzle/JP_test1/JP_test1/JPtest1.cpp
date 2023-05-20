d48f226c81b082288af81201e293167b30a98ab7
cjh-19 <gns8812@naver.com>
2023-05-20 18:36:21 +0900

    성공
    솔루션의 개수가 4개 이상 나올 수 있는지 체크할 것

--- Scramble Square Puzzle/JP_test1/JP_test1/JPtest1.cpp
+++ Scramble Square Puzzle/JP_test1/JP_test1/JPtest1.cpp
@@ -1,118 +1,375 @@
-/// Scramble Square Puzzle Problem ///
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 struct PuzzlePiece {
-	vector<int> sides; // 각 면에 대한 값
-	int orientation; // 회전 상태
-
-	//PuzzlePiece(int orientation, const vector<int>& sides) : orientation(orientation), sides(sides) {}
+    int id;
+    int sides[4];
+    int orientation;
 };
 
+int solutionCount = 0;
+
 bool nextLocal(int x, int width) {
-	if (x < width - 1)
-		return true;
-	return false;
+    if (x < width - 1) {
+        return true;
+    }
+    return false;
 }
 
 int getSide(PuzzlePiece piece, int side) {
-	int num = piece.orientation;
-	return piece.sides[(num + side - 1) % 4];
+    int num = piece.orientation;
+    return piece.sides[(num + side) % 4];
 }
 
-// Promising 한지 검사하는 부분
-bool checkMove(const vector<vector<PuzzlePiece>>& puzzle, const PuzzlePiece& piece, int x, int y) { // x와 y는 현재 위치
-	if (x > 0) { // 왼쪽에 인접한 면이 있는 경우
-		//int side1 = puzzle[x - 1][y].sides[1]; // 오른쪽
-		//int side2 = piece.sides[3]; // 왼쪽
-		int side1 = getSide(puzzle[x - 1][y], 1);
-		int side2 = getSide(piece, 3);
-		if (side1 != side2)
-			return false;
-	}
-	if (y > 0) {
-		//int side1 = puzzle[x][y - 1].sides[2]; // 아래쪽
-		//int side2 = piece.sides[0]; // 위쪽
-		int side1 = getSide(puzzle[x][y - 1], 2);
-		int side2 = getSide(piece, 0);
-		if (side1 != side2)
-			return false;
-	}
-	return true;
+bool checkMove(vector<vector<PuzzlePiece>>& puzzle, PuzzlePiece piece, int x, int y) {
+    if (x > 0) {
+        int side1 = getSide(puzzle[x - 1][y], 1);
+        int side2 = getSide(piece, 3);
+        if (!(side1 == 1 && side2 == 2) && !(side1 == 2 && side2 == 1) && !(side1 == 3 && side2 == 4) && !(side1 == 4 && side2 == 3) &&
+            !(side1 == 5 && side2 == 6) && !(side1 == 6 && side2 == 5) && !(side1 == 7 && side2 == 8) && !(side1 == 8 && side2 == 7)) {
+            return false;
+        }
+    }
+    if (y > 0) {
+        int side1 = getSide(puzzle[x][y - 1], 2);
+        int side2 = getSide(piece, 0);
+        if (!(side1 == 1 && side2 == 2) && !(side1 == 2 && side2 == 1) && !(side1 == 3 && side2 == 4) && !(side1 == 4 && side2 == 3) &&
+            !(side1 == 5 && side2 == 6) && !(side1 == 6 && side2 == 5) && !(side1 == 7 && side2 == 8) && !(side1 == 8 && side2 == 7)) {
+            return false;
+        }
+    }
+    return true;
 }
 
-bool solvePuzzle(vector<vector<PuzzlePiece>>& puzzle, vector<PuzzlePiece>& pieces, int x, int y, int& solutionCount) {
-	if (y == puzzle.size()) { // 해를 찾았을 경우 출력
-		solutionCount++;
-		if (solutionCount == 1)
-			cout << solutionCount << endl;
-		else
-			cout << endl;
+bool solvePuzzle(vector<vector<PuzzlePiece>>& puzzle, vector<PuzzlePiece>& pieces, int x, int y) {
+    if (y == 3) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cout << puzzle[j][i].id << "(" << puzzle[j][i].orientation << ") ";
+            }
+            cout << endl;
+        }
+        return true;
+    }
 
-		for (const auto& row : puzzle) {
-			for (const auto& piece : row) {
-				cout << piece.orientation + 1 << "(" << piece.orientation << ") ";
-			}
-			cout << endl;
-		}
-		return true;
-	}
+    for (int i = 0; i < 9; i++) {
+        PuzzlePiece temp = pieces[i];
 
-	for (auto& piece : pieces) {
-		if (piece.orientation != 0) continue;
+        if (temp.orientation != 0) {
+            continue;
+        }
 
-		int curr_x = x;
-		int curr_y = y;
+        int curr_x = x;
+        int curr_y = y;
 
-		if (nextLocal(x, puzzle.size()))//(x < puzzle.size() - 1)
-			x++;
-		else {
-			y++;
-			x = 0;
-		}
+        
 
-		for (int j = 0; j < 4; j++) {
-			piece.orientation = j;
-			if (checkMove(puzzle, piece, curr_x, curr_y)) {
-				if (curr_x < puzzle.size() && curr_y < puzzle.size())
-					puzzle[curr_x][curr_y] = piece;
-				if (solvePuzzle(puzzle, pieces, x, y, solutionCount))
-					return true;
-				else
-					piece.orientation = 0;
-			}
-		}
-	}
-	return false;
+        if (nextLocal(x, 3)) {
+            x++;
+        }
+        else {
+            y++;
+            x = 0;
+        }
+
+        for (int j = 1; j <= 4; j++) {
+            temp.orientation = j;
+            if (checkMove(puzzle, temp, curr_x, curr_y)) {
+                puzzle[curr_x][curr_y] = temp;
+                //solutionCount += solvePuzzle(puzzle, pieces, x, y);
+                if (solvePuzzle(puzzle, pieces, x, y)) {
+                    solutionCount++;
+                    return true;//solutionCount++;
+                }
+                else
+                    temp.orientation = 0;
+                //puzzle[curr_x][curr_y].orientation = 0;
+            }
+        }
+    }
+
+    return false; // solutionCount;
 }
 
-void findSolutions(vector<PuzzlePiece>& pieces, int width) {
-	vector<vector<PuzzlePiece>> puzzle(3, vector<PuzzlePiece>(3));
-	int solutionCount = 0;
-	solvePuzzle(puzzle, pieces, 0, 0, solutionCount);
+void findSoln(vector<PuzzlePiece> pieces, int width) {
+    vector<vector<PuzzlePiece>> puzzle(width, vector<PuzzlePiece>(width));
+    int num = 0;
+    if (solvePuzzle(puzzle, pieces, 0, 0)) {
+        cout << "Number of solutions: " << solutionCount << endl;
+    }
+
 }
 
 int main() {
-	vector<PuzzlePiece> allPieces;
-	for (int i = 0; i < 9; i++) {
-		PuzzlePiece puzzlePiece;
-		for (int j = 0; j < 4; j++) {
-			int num;
-			cin >> num;
-			puzzlePiece.sides.push_back(num);
-		}
-		puzzlePiece.orientation = 0;
-		allPieces.push_back(puzzlePiece);
-	}
+    vector<PuzzlePiece> pieces(9);
+    for (int i = 0; i < 9; i++) {
+        int s1, s2, s3, s4;
+        cin >> s1 >> s2 >> s3 >> s4;
+        pieces[i].sides[0] = s1;
+        pieces[i].sides[1] = s2;
+        pieces[i].sides[2] = s3;
+        pieces[i].sides[3] = s4;
+        pieces[i].id = i;
+        pieces[i].orientation = 0;
+    }
 
-	findSolutions(allPieces, 3);
-
-	return 0;
+    findSoln(pieces, 3);
+    return 0;
 }
+
+
+
+
+
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//
+//struct PuzzlePiece {
+//    string sides[4];
+//    int orientation;
+//};
+//
+//vector<PuzzlePiece> P;
+//
+//bool nextLocal(int x, int width) {
+//    if (x < width - 1) {
+//        return true;
+//    }
+//    return false;
+//}
+//
+//string getSide(PuzzlePiece piece, int side) {
+//    int num = piece.orientation;
+//    return piece.sides[(num + side - 1) % 4];
+//}
+//
+//bool checkMove(PuzzlePiece puzzle[3][3], PuzzlePiece piece, int x, int y) {
+//    if (x > 0) {
+//        string side1 = getSide(puzzle[x - 1][y], 1);
+//        string side2 = getSide(piece, 3);
+//        cout << side1 << " " << side2 << x << y << "\n";
+//        if (side1[0] != side2[0]) {
+//            return false;
+//        }
+//        if (side1[1] != side2[1]) {
+//            return false;
+//        }
+//    }
+//    if (y > 0) {
+//        string side1 = getSide(puzzle[x][y - 1], 1);
+//        string side2 = getSide(piece, 0);
+//        cout << side1 << " " << side2 << x << y << "\n";
+//
+//        if (side1[0] != side2[0]) {
+//            return false;
+//        }
+//        if (side1[1] != side2[1]) {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
+//
+//bool solvePuzzle(PuzzlePiece puzzle[3][3], PuzzlePiece pieces[], int x, int y) {
+//    if (y == 3) {
+//        //for (int i = 0; i < 9; i++) {
+//        //    for (int j = 0; j < 4; j++) {
+//        //        cout << pieces[i].sides[j] << " ";
+//        //    }
+//        //    cout << "\n";
+//        //}
+//        return true;
+//    }
+//
+//    for (int i = 0; i < 9; i++) {
+//        PuzzlePiece temp = pieces[i];
+//
+//        if (temp.orientation != 0) {
+//            continue;
+//        }
+//
+//        int curr_x = x;
+//        int curr_y = y;
+//
+//        if (nextLocal(x, 3)) {
+//            x++;
+//        }
+//        else {
+//            y++;
+//            x = 0;
+//        }
+//
+//        for (int j = 1; j <= 4; j++) {
+//            temp.orientation = j;
+//            if (checkMove(puzzle, temp, curr_x, curr_y)) {
+//                puzzle[curr_x][curr_y] = temp;
+//                if (solvePuzzle(puzzle, pieces, x, y)) {
+//                   // P[x][y].sides = pieces.sides;
+//                    return true;
+//                }
+//                else {
+//                    temp.orientation = 0;
+//                }
+//            }
+//        }
+//    }
+//
+//    return false;
+//}
+//
+//void findSoln(PuzzlePiece pieces[], int width) {
+//    PuzzlePiece soln[3][3];
+//    solvePuzzle(soln, pieces, 0, 0);
+//}
+//
+//int main() {
+//    PuzzlePiece pieces[9];
+//    pieces[0] = { {"dl", "zr", "er", "el"}, 0 };
+//    pieces[1] = { {"zl", "dl", "tr", "er"}, 0 };
+//    pieces[2] = { {"dl", "tr", "el", "zl"}, 0 };
+//    pieces[3] = { {"zr", "tr", "el", "dr"}, 0 };
+//    pieces[4] = { {"tr", "zr", "dr", "er"}, 0 };
+//    pieces[5] = { {"dl", "zl", "tl", "er"}, 0 };
+//    pieces[6] = { {"tl", "zr", "dl", "er"}, 0 };
+//    pieces[7] = { {"zl", "dr", "tl", "zr"}, 0 };
+//    pieces[8] = { {"tl", "zl", "dl", "er"}, 0 };
+//
+//    findSoln(pieces, 3);
+//    //for (int i = 0; i < 9; i++) {
+//    //    for (int j = 0; j < 4; j++) {
+//    //       cout << pieces[i].sides[j] << " ";
+//    //    }
+//    //    cout << "\n";
+//    //}
+//    return 0;
+//}
+
+
+
+
+
+
+/// Scramble Square Puzzle Problem ///
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//struct PuzzlePiece {
+//	vector<int> sides; // 각 면에 대한 값
+//	int orientation; // 회전 상태
+//
+//	//PuzzlePiece(int orientation, const vector<int>& sides) : orientation(orientation), sides(sides) {}
+//};
+//
+//bool nextLocal(int x, int width) {
+//	if (x < width - 1)
+//		return true;
+//	return false;
+//}
+//
+//int getSide(PuzzlePiece piece, int side) {
+//	int num = piece.orientation;
+//	return piece.sides[(num + side - 1) % 4];
+//}	
+//
+//// Promising 한지 검사하는 부분, 퍼즐을 해당 위치에 놓을 수 있는지 오른쪽과 위쪽을 검사.
+//bool checkMove(const vector<vector<PuzzlePiece>>& puzzle, const PuzzlePiece& piece, int x, int y) { // x와 y는 현재 위치
+//	if (x > 0) { // 왼쪽에 인접한 면이 있는 경우
+//		//int side1 = puzzle[x - 1][y].sides[1]; // 오른쪽
+//		//int side2 = piece.sides[3]; // 왼쪽
+//		int side1 = getSide(puzzle[x - 1][y], 1);
+//		int side2 = getSide(piece, 3);
+//		int sum = side1 + side2;
+//		if (sum != 3 && sum != 7 && sum != 11 && sum != 16)//(side1 !=  side2)
+//			return false;
+//	}
+//	if (y > 0) {
+//		//int side1 = puzzle[x][y - 1].sides[2]; // 아래쪽
+//		//int side2 = piece.sides[0]; // 위쪽
+//		int side1 = getSide(puzzle[x][y - 1], 2);
+//		int side2 = getSide(piece, 0);
+//		int sum = side1 + side2;
+//		if (sum != 3 && sum != 7 && sum != 11 && sum != 16)//(side1 != side2)
+//			return false;
+//	}
+//	return true;
+//}
+//
+//bool solvePuzzle(vector<vector<PuzzlePiece>>& puzzle, vector<PuzzlePiece>& pieces, int x, int y, int& solutionCount) {
+//	if (y == puzzle.size()) { // 해를 찾았을 경우 출력
+//		solutionCount++;
+//		if (solutionCount == 1)
+//			cout << solutionCount << endl;
+//		else
+//			cout << endl;
+//
+//		for (const auto& row : puzzle) {
+//			for (const auto& piece : row) {
+//				cout << piece.orientation + 1 << "(" << piece.orientation << ") ";
+//			}
+//			cout << endl;
+//		}
+//		return true;
+//	}
+//
+//	for (auto& piece : pieces) {
+//		if (piece.orientation != 0) continue;
+//
+//		int curr_x = x;
+//		int curr_y = y;
+//
+//		if (nextLocal(x, puzzle.size()))//(x < puzzle.size() - 1)
+//			x++;
+//		else {
+//			y++;
+//			x = 0;
+//		}
+//
+//		for (int j = 0; j < 4; j++) {
+//			piece.orientation = j;
+//			if (checkMove(puzzle, piece, curr_x, curr_y)) {
+//				if (curr_x < puzzle.size() && curr_y < puzzle.size())
+//					puzzle[curr_x][curr_y] = piece;
+//				if (solvePuzzle(puzzle, pieces, x, y, solutionCount))
+//					return true;
+//				else
+//					piece.orientation = 0;
+//			}
+//		}
+//	}
+//	return false;
+//}
+//
+//void findSolutions(vector<PuzzlePiece>& pieces, int width) {
+//	vector<vector<PuzzlePiece>> puzzle(3, vector<PuzzlePiece>(3));
+//	int solutionCount = 0;
+//	solvePuzzle(puzzle, pieces, 0, 0, solutionCount);
+//}
+//
+//int main() {
+//	vector<PuzzlePiece> allPieces;
+//	for (int i = 0; i < 9; i++) {
+//		PuzzlePiece puzzlePiece;
+//		for (int j = 0; j < 4; j++) {
+//			int num;
+//			cin >> num;
+//			puzzlePiece.sides.push_back(num);
+//		}
+//		puzzlePiece.orientation = 0;
+//		allPieces.push_back(puzzlePiece);
+//	}
+//
+//	findSolutions(allPieces, 3);
+//
+//	return 0;
+//}
 
 
 
@@ -339,7 +596,7 @@ int main() {
 //
 //        pieces.emplace_back(i, nums); // Piece 객체가 생성되고 해당 객체는 piece 벡터에 추간된다.
 //    }
-//
+//	
 //    solve(0);
 //
 //    return 0;
--- Scramble Square Puzzle/Scramble_Test/Scramble_Test/test1.cpp
+++ Scramble Square Puzzle/Scramble_Test/Scramble_Test/test1.cpp
@@ -1,41 +1,345 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
 
-enum class Part {
-	HEAD = 1,
-	TAIL = -1
+struct PuzzlePiece {
+	int id; // 조각 번호
+	vector<int> sides; // 각 면의 값
+	int orientation; // 회전 상태
+	int x; // 열의 위치
+	int y; // 행의 위치
+	// (x,y) = x열 y행
+	PuzzlePiece(int id, const vector<int>& sides, int orientation, int x, int y) : id(id), sides(sides), orientation(orientation), x(x), y(y) {}
 };
 
-enum class PartIndex {
-	ONE = 1,
-	TWO = 2,
-	THREE = 3,
-	FOUR = 4
-};
+int count_solution = 0; // 해결할 수 있는 경우의 수
+vector<PuzzlePiece> pieces; // 입력으로 주어진 9개의 이미지 조각을 저장하는 벡터
+vector<int> solution(9); // 현재까지 구성된 조각들의 순서를 저장하는 벡터
+vector<bool> used(9, false); // 이미 사용된 조각을 표시하는 벡터
 
-class Side {
-private:
-	Part part;
-	PartIndex index;
+// 퍼즐이 다음 위치로 이동할 수 있는지 확인하는 함수
+// 가장 오른쪽열에 위치해있다면 이동할 수 없으므로 false 반환
+bool nextLocal(int x, int width) {
+	if (x < width - 1)
+		return true;
+	return false;
+}
+
+int getSide(PuzzlePiece piece, int side) {
+	int num = piece.orientation;
+	return piece.sides[(num + side - 1) % 4];
+}
+
+bool checkMove(vector<vector<PuzzlePiece>> puzzle, PuzzlePiece piece, int x, int y) {
+	if (x > 0) {
+		int side1 = getSide(puzzle[x - 1][y], 1);
+		int side2 = getSide(piece, 3);
+		if (!(side1 == 1 && side2 == 2) && !(side1 == 2 && side2 == 1) && !(side1 == 3 && side2 == 4) && !(side1 == 4 && side2 == 3) &&
+			!(side1 == 5 && side2 == 6) && !(side1 == 6 && side2 == 5) && !(side1 == 7 && side2 == 8) && !(side1 == 8 && side2 == 7)) {
+			return false;
+		}
+	}
+	if (y > 0) {
+		int side1 = getSide(puzzle[x][y - 1], 2);
+		int side2 = getSide(piece, 0);
+		if (!(side1 == 1 && side2 == 2) && !(side1 == 2 && side2 == 1) && !(side1 == 3 && side2 == 4) && !(side1 == 4 && side2 == 3) &&
+			!(side1 == 5 && side2 == 6) && !(side1 == 6 && side2 == 5) && !(side1 == 7 && side2 == 8) && !(side1 == 8 && side2 == 7)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int getNumRows(vector<vector<PuzzlePiece>>& puzzle) {
+	//if (puzzle.empty()) {
+	//	return 0;
+	//}
+	cout << "x값 : " << puzzle.size() << "\n";
+	return puzzle.size();
+}
+
+int getNumCols(vector<vector<PuzzlePiece>>& puzzle, int x) {
 	
-public:
-	Side(Part part, PartIndex index) : part(part), index(index) {}
+	return puzzle[x].size();
+}
 
-	int getValue() const {
-		return static_cast<int>(part) * static_cast<int>(index);
+bool solvePuzzle(vector<vector<PuzzlePiece>>& puzzle, vector<PuzzlePiece>& pieces, int x, int y) {
+
+	if (x == 3 && y == 3) {
+		count_solution++;
+		for (const auto& row : puzzle) {
+			for (const auto& piece : row) {
+				cout << piece.orientation + 1 << "(" << piece.orientation << ") ";
+			}
+			cout << endl;
+		}
+		//for (const auto& piece : pieces)
+		//	cout << piece.orientation + 1 << "(" << piece.orientation << ")";
+		//	
+		//cout << "\n";
+		return true;
 	}
 
-	bool canJoin(const Side& side) const {
-		if (&side == nullptr)
-			return true;
-		return getValue() + side.getValue() == 0;
+	for (int i = 0; i < pieces.size(); i++) {
+		PuzzlePiece temp = pieces[i];
+
+		if (temp.orientation != 0) {
+			continue;
+		}
+
+		int curr_x = x;
+		int curr_y = y;
+
+		if (nextLocal(x, getNumCols(puzzle, x))) {
+			x++;
+		}
+		else {
+			y++;
+			x = 0;
+		}
+
+		for (int j = 1; j <= 4; j++) {
+			temp.orientation = j;
+			if (checkMove(puzzle, temp, curr_x, curr_y)) {
+				puzzle[curr_x][curr_y] = temp;
+				if (solvePuzzle(puzzle, pieces, x, y)) {
+					return true;
+				}
+				else {
+					temp.orientation = 0;
+				}
+			}
+		}
 	}
-};
+
+	return false;
+}
+
+vector<vector<PuzzlePiece>> findSoln(vector<PuzzlePiece> pieces, int width) {
+	vector<vector<PuzzlePiece>> soln;
+	solvePuzzle(soln, pieces, 0, 0);
+
+	cout << count_solution << "\n";
+
+	return soln;
+}
 
 int main() {
+	// 퍼즐 조각 받기
+	int x = 0, y = 0;
+	for (int i = 0; i < 9; i++) {
+		vector<int> sides(4);
+		for (int j = 0; j < 4; j++)
+			cin >> sides[j];
+		pieces.emplace_back(i, sides, 0, x, y); // Piece 객체가 생성되고 해당 객체에 pieces 벡터가 추가된다.
+		x++;
+		if (x == 4) {
+			x = 0;
+			y++;
+		}
+	}
+
+	findSoln(pieces, 3);
+	// solve(0);
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//enum class Part {
+//	HEAD = 1,
+//	TAIL = -1
+//};
+//
+//enum class PartIndex {
+//	ONE = 1,
+//	TWO = 2,
+//	THREE = 3,
+//	FOUR = 4
+//};
+//
+//class Side {
+//private:
+//	Part part;
+//	PartIndex index;
+//	
+//public:
+//	Side(Part part, PartIndex index) : part(part), index(index) {}
+//
+//	int getValue() const {
+//		return static_cast<int>(part) * static_cast<int>(index);
+//	}
+//
+//	bool canJoin(const Side& side) const {
+//		if (&side == nullptr)
+//			return true;
+//		return getValue() + side.getValue() == 0;
+//	}
+//
+//	string toString() const {
+//		stringstream ss;
+//		ss << static_cast<int>(part) << "-" << static_cast<int>(index);
+//		return ss.str();
+//	}
+//
+//	Side clone() const {
+//		return Side(part, index);
+//	}
+//};
+//
+//class Card {
+//private:
+//	static int counter; // 정적 멤버 변수를 사용하여 카드 이름의 유일성 보장
+//	string name;
+//	Side top;
+//	Side left;
+//	Side bottom;
+//	Side right;
+//
+//public:
+//	Card(Side top, Side left, Side bottom, Side right)
+//		: name("Card" + to_string(counter++)), top(top), left(left), bottom(bottom), right(right) {}
+//
+//	// 복사 생성자
+//	Card(const Card& other)
+//		: name("Card" + to_string(counter++)),
+//		top(other.top.clone()), left(other.left.clone()), bottom(other.bottom.clone()),right(other.right.clone()) {}
+//	
+//	// 대입 연산자
+//	Card& operator=(const Card& other) {
+//		if (this != &other) {
+//			name = "Card" + to_string(counter++);
+//			top = other.top.clone();
+//			left = other.left.clone();
+//			bottom = other.bottom.clone();
+//			right = other.right.clone();
+//		}
+//		return *this;
+//	}
+//
+//	bool isCompatible(const Card& leftCard, const Card& topCard) const {
+//		if (&leftCard == nullptr && &topCard != nullptr) {
+//			return top.canJoin(topCard.bottom);
+//		}
+//		else if (&left != nullptr && &topCard == nullptr) {
+//			return left.canJoin(leftCard.right);
+//		}
+//		else if (&leftCard == nullptr && &topCard == nullptr) {
+//			return true;
+//		}
+//		return left.canJoin(leftCard.right) && top.canJoin(topCard.bottom);
+//	}
+//
+//	string toString() const {
+//		return name;
+//	}
+//
+//	Card clone() const {
+//		return Card(top.clone(), left.clone(), bottom.clone(), right.clone());
+//	}
+//};
+//
+//int Card::counter = 1; // 정적 멤버 변수 초기화
+//
+//class DraftSpace {
+//private:
+//	vector<vector<Card>> cards;
+//	int dim;
+//
+//	int getRow(int pos) const {
+//		return pos / dim;
+//	}
+//
+//	int getColumn(int pos) const {
+//		return pos % dim;
+//	}
+//
+//	const Card* getLeftCard(int pos) const {
+//		int r, c;
+//		if (pos >= dim * dim) {
+//			return nullptr;
+//		}
+//		r = getRow(pos);
+//		c = getColumn(pos);
+//		return &cards[r][c - 1];
+//	}
+//
+//	const Card* getTopCard(int pos) const {
+//		int r, c;
+//		if (pos >= dim * dim) {
+//			return nullptr;
+//		}
+//		r = getRow(pos);
+//		c = getColumn(pos);
+//		return &cards[r - 1][c];
+//	}
+//
+//public:
+//	DraftSpace(int dim) : dim(dim) {
+//		cards.resize(dim, vector<Card>(dim));
+//	}
+//
+//	bool tryAdd(const Card& card, int pos) {
+//		const Card* l = getLeftCard(pos);
+//		const Card* t = getTopCard(pos);
+//		int r = getRow(pos);
+//		int c = getColumn(pos);
+//
+//		if (card.isCompatible(*l, *t)) {
+//			cards[r][c] = card.clone();
+//			return true;
+//		}
+//		return false;
+//	}
+//};
+//
+//int main() {
+//	// Card 객체 생성
+//	Card card1(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card2(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card3(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card4(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card5(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card6(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card7(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card8(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//	Card card9(Side(Part::HEAD, PartIndex::ONE), Side(Part::TAIL, PartIndex::TWO),
+//		Side(Part::HEAD, PartIndex::THREE), Side(Part::TAIL, PartIndex::FOUR));
+//
+//	// DraftSpace 객체 생성
+//	DraftSpace draftSpace(3);
+//
+//	// 카드 추가 시도 이 부분 모르겠음
+//
+//	// 애초에 이렇게 코드 짜는건 진짜 아닌 것 같다.
+//
+//	return 0;
+//}
