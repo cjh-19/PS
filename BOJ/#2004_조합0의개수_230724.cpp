74cf401398b6c9789bb1e80cbc2b7f80fa6fcd78
cjh-19 <gns8812@naver.com>
2023-07-24 18:59:40 +0900

    BOJ

---
+++ BOJ/#2004_조합0의개수_230724.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	long long n, m;
+	int cnt2 = 0, cnt5 = 0;
+	cin >> n >> m;
+	for (long long i = 5; i <= n; i*=5) {
+		cnt5 += n / i;
+	}
+	for (long long i = 5; i <= n - m; i *= 5) {
+		cnt5 -= (n - m) / i;
+	}
+	for (long long i = 5; i <= m; i*=5) {
+		cnt5 -= m / i;
+	}
+	for (long long i = 2; i <= n; i *= 2) {
+		cnt2 += n / i;
+	}
+	for (long long i = 2; i <= n - m; i *= 2) {
+		cnt2 -= (n - m) / i;
+	}
+	for (long long i = 2; i <= m; i *= 2) {
+		cnt2 -= m / i;
+	}
+	cout << min(cnt2, cnt5) << "\n";
+	return 0;
+}
