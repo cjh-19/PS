3d75a1fd7d2b01cf6c86024ddaee634e8ad26424
cjh-19 <gns8812@naver.com>
2023-08-09 11:38:03 +0900

    BOJ

--- BOJ/#17103_골드바흐파티션_230809.cpp
+++ BOJ/#17103_골드바흐파티션_230809.cpp
@@ -7,27 +7,23 @@ int arr[MAX];
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	for (int i = 2; i < MAX; i++) {
+	for (int i = 2; i < MAX; i++)
 		arr[i] = i;
-	}
-	for (int i = 2; i < MAX; i++) {
+	for (int i = 2; i * i < MAX; i++) {
 		if (arr[i] == 0)
 			continue;
-		for (int j = i * 2; j < MAX; j += i) {
+		for (int j = i * i; j < MAX; j += i)
 			arr[j] = 0;
-		}
 	}
 	int Num;
 	cin >> Num;
 	for (int i = 0; i < Num; i++) {
 		int N, count = 0;
 		cin >> N;
-		for (int i = 2; i <= N / 2; i++) {
-			if (arr[i] != 0) {
+		for (int i = 2; i <= N / 2; i++)
+			if (arr[i] != 0)
 				if (arr[N-i] + arr[i] == N)
 					count++;
-			}
-		}
 		cout << count << "\n";
 	}
 	return 0;
