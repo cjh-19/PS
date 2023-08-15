50cb9c24e7016c57c7e62a7a6b28bf22c54829e3
cjh-19 <gns8812@naver.com>
2023-08-15 17:05:01 +0900

    BOJ

--- BOJ/#1463_1로만들기_230814.cpp
+++ BOJ/#1463_1로만들기_230814.cpp
@@ -2,21 +2,16 @@
 
 using namespace std;
 
+#define MAX 100001
+
+int dp[MAX] = { 0, };
+
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	int Num, count = 0;
 	cin >> Num;
-	while (Num != 1) {
-		if (Num % 3 == 0)
-			Num /= 3;
-		else if ((Num - 1) % 3 == 0)
-			Num -= 1;
-		else if (Num % 2 == 0)
-			Num /= 2;
-		else
-			Num -= 1;
-		count++;
-	}
+
+
 	cout << count << "\n";
 
 	return 0;
