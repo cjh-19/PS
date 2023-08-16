890a6e0a002b3c97620ada40b8e4bb380e8eaac8
cjh-19 <gns8812@naver.com>
2023-08-16 14:31:45 +0900

    BOJ

--- BOJ/#1463_1로만들기_230814.cpp
+++ BOJ/#1463_1로만들기_230814.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 
-#define MAX 100001
-
+#define MAX 1000001
 int dp[MAX] = { 0, };
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	int Num, count = 0;
+	int Num;
 	cin >> Num;
-
-
-	cout << count << "\n";
+	for (int i = 2; i <= Num; i++) {
+		if (i % 3 == 0 && i % 2 == 0) {
+			dp[i] = min(dp[i / 3], dp[i / 2]) + 1;
+		}
+		else if (i % 3 == 0)
+			dp[i] = min(dp[i / 3], dp[i - 1]) + 1;
+		else if (i % 2 == 0)
+			dp[i] = min(dp[i / 2], dp[i - 1]) + 1;
+		else
+			dp[i] = dp[i - 1] + 1;
+	}
+	cout << dp[Num] << "\n";
 
 	return 0;
 }
