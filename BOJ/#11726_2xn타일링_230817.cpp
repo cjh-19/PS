f63c20b01fb228d758fccbae8f5a036eb0fe2e63
cjh-19 <gns8812@naver.com>
2023-08-17 17:25:18 +0900

    BOJ

---
+++ BOJ/#11726_2xn타일링_230817.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int num;
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+
+	}
+
+	return 0;
+}
