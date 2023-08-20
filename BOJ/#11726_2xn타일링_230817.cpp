304979abf4f33bf5e0f1ed0663717d6b640a9223
cjh-19 <gns8812@naver.com>
2023-08-20 23:48:47 +0900

    BOJ

--- BOJ/#11726_2xn타일링_230817.cpp
+++ BOJ/#11726_2xn타일링_230817.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	int num;
+	int num, count = 0;
 	cin >> num;
 	int dp[10000] = {0,};
 	for (int i = 0; i < num; i++) {
-
+		
 	}
+	cout << count % 10007 << "\n"
 
 	return 0;
 }
