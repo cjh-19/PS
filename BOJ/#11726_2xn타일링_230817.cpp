cb7fd4541b8e5bda93b89b4e45c1afd05425f835
cjh-19 <gns8812@naver.com>
2023-08-23 23:43:33 +0900

    BOJ

--- BOJ/#11726_2xn타일링_230817.cpp
+++ BOJ/#11726_2xn타일링_230817.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	int num, count = 0;
+	int num;
 	cin >> num;
-	int dp[10000] = {0,};
-	for (int i = 0; i < num; i++) {
-		
+	vector<int> dp;
+	dp.push_back(1);
+	dp.push_back(2);
+	for (int i = 2; i < num; i++) {
+		dp.push_back((dp[i - 1] + dp[i - 2]) % 10007);
 	}
-	cout << count % 10007 << "\n"
+	cout << dp[num - 1] << "\n";
 
 	return 0;
 }
