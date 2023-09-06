ceef44bbff1f0a17f7a885f7428af75d20ec4d0e
cjh-19 <gns8812@naver.com>
2023-09-06 21:53:12 +0900

    BOJ

---
+++ BOJ/#11052_카드구매하기_230906.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int Num;
+	cin >> Num;
+	vector<int> card;
+	for (int i = 0; i < Num; i++) {
+		int n;
+		cin >> n;
+		card.push_back(n);
+	}
+	vector<int> dp;
+	dp.push_back(0);
+	int max = 0;
+	for (int i = 1; i <= Num; i++) {
+		dp.push_back(0);
+		for (int j = 0; j < i; j++) {
+			if (max < card[Num - j - 1] + dp[j]) {
+				max = card[Num - j - 1] + dp[j];
+				dp[i] = max;
+			}
+		}
+	}
+	cout << dp[Num] << "\n";
+
+	return 0;
+}
