7990abd2c4152306057150e6dd545bc5fc85c1fd
cjh-19 <gns8812@naver.com>
2023-09-09 20:05:06 +0900

    BOJ

--- BOJ/#15990_1,2,3더하기5_230909.cpp
+++ BOJ/#15990_1,2,3더하기5_230909.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
+
+long long dp[100001][3] = { 0, };
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
@@ -10,16 +11,15 @@ int main() {
 	for (int i = 0; i < T; i++) {
 		int num;
 		cin >> num;
-		vector<long long> dp;
-		dp.push_back(1);
-		dp.push_back(1);
-		dp.push_back(3);
-		dp.push_back(3);
-		dp.push_back(5);
-		for (int j = 5; j < num; j++) {
-			dp.push_back((dp[j - 1] + dp[j - 2] + dp[j - 3] - 3) % 1000000009);
+		dp[0][0] = 1;
+		dp[1][1] = 1;
+		dp[2][0] = 1; dp[2][1] = 1; dp[2][2] = 1;
+		for (int j = 3; j < num; j++) {
+			dp[j][0] = dp[j - 1][1] + dp[j - 1][2];
+			dp[j][1] = dp[j - 1][0] + dp[j - 1][2];
+			dp[j][2] = dp[j - 1][0] + dp[j - 1][1];
 		}
-		cout << dp[num - 1] << "\n";
+		cout << (dp[num - 1][0] + dp[num - 1][1] + dp[num - 1][2]) % 1000000009 << "\n";
 	}
 
 	return 0;
