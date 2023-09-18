f312503dfd034b5dcae900459e0b04d032f197e9
cjh-19 <gns8812@naver.com>
2023-09-18 23:45:26 +0900

    BOJ

--- BOJ/#14002_가장긴증가하는부분수열4_230914.cpp
+++ BOJ/#14002_가장긴증가하는부분수열4_230914.cpp
@@ -8,6 +8,7 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> arr, dp;
+	vector<vector<int>> vt(1000);
 	for (int i = 0; i < N; i++) {
 		int num;
 		cin >> num;
@@ -16,13 +17,27 @@ int main() {
 	dp.push_back(1);
 	for (int i = 1; i < N; i++) {
 		dp.push_back(0);
-		int max = 0;
 		for (int j = 0; j < i; j++) {
-			if (arr[i] > arr[j] && dp[i] < dp[j])
+			if (arr[i] > arr[j] && dp[i] < dp[j]) {
 				dp[i] = dp[j];
+			}
 		}
 		dp[i]++;
+		vt[i].push_back(arr[i]);
+	}
+	int max = 0, k = 0;;
+	for (int i = 0; i < N; i++) {
+		if (max < dp[i]) {
+			max = dp[i];
+			k = i;
+		}
+	}
+	cout << max << "\n";
+	for (int i = 0; i < vt[k].size(); i++) {
+		cout << vt[k][i] << " ";
 	}
 
 	return 0;
 }
+
+// 아이디어 : 2차원 배열을 통해서 각 값에 수를 저장
