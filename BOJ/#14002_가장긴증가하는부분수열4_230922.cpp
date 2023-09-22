1e80ab5002a78b49526c3f5e3b3328e1d3ab41c4
cjh-19 <gns8812@naver.com>
2023-09-22 18:03:11 +0900

    BOJ

--- BOJ/#14002_가장긴증가하는부분수열4_230922.cpp
+++ BOJ/#14002_가장긴증가하는부분수열4_230922.cpp
@@ -7,29 +7,25 @@ int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	int N;
 	cin >> N;
-	vector<int> arr, dp;
-	vector<vector<int>> vt(1000);
+	vector<int> arr;
+	vector<vector<int>> vt(1001);
 	for (int i = 0; i < N; i++) {
 		int num;
 		cin >> num;
 		arr.push_back(num);
 	}
-	dp.push_back(1);
-	for (int i = 1; i < N; i++) {
-		dp.push_back(0);
+	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < i; j++) {
-			if (arr[i] > arr[j] && dp[i] < dp[j]) {
-				dp[i] = dp[j];
+			if (arr[i] > arr[j] && vt[i].size() < vt[j].size()) {
 				vt[i].push_back(arr[j]);
 			}
 		}
-		dp[i]++;
 		vt[i].push_back(arr[i]);
 	}
 	int max = 0, maxnum = 0;;
 	for (int i = 0; i < N; i++) {
-		if (max < dp[i]) {
-			max = dp[i];
+		if (max < vt[i].size()) {
+			max = vt[i].size();
 			maxnum = i;
 		}
 	}
