051c98d6646971461922e3c41f14eace43788d62
cjh-19 <gns8812@naver.com>
2023-09-28 23:50:47 +0900

    BOJ

--- BOJ/#14002_가장긴증가하는부분수열4_230922.cpp
+++ BOJ/#14002_가장긴증가하는부분수열4_230922.cpp
@@ -7,21 +7,18 @@ int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	int N;
 	cin >> N;
-	vector<int> arr;
-	vector<vector<int>> vt(1001);
-	for (int i = 0; i < N; i++) {
-		int num;
-		cin >> num;
-		arr.push_back(num);
-	}
-	for (int i = 0; i < N; i++) {
-		int j;
-		for (j = 0; j < i; j++) {
+	vector<int> arr(N + 1);
+	vector<vector<int>> vt(N + 1);
+
+	vt[0].push_back(0);
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+		for (int j = 1; j < i; j++) {
 			if (arr[i] > arr[j] && vt[i].size() < vt[j].size()) {
 				vt[i].push_back(arr[j]);
 			}
 		}
-		vt[i].push_back(arr[j]);
+		vt[i].push_back(arr[i]);
 	}
 	int max = 0, maxnum = 0;;
 	for (int i = 0; i < N; i++) {
@@ -37,6 +34,3 @@ int main() {
 
 	return 0;
 }
-
-// 5 3 1 2 7 을 인자로 받았을 때 5 2 7 이 출력되는 이유를 모르겠음
-// 논리상 정확한 것 같은데 이유를 모름 -> 코드의 구조를 다시 짜야할 듯 함
