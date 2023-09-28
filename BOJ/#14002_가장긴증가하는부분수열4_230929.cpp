79fdd1020abdd401f15d1975aade3a18ead6ac7b
cjh-19 <gns8812@naver.com>
2023-09-29 00:14:02 +0900

    BOJ

--- BOJ/#14002_가장긴증가하는부분수열4_230929.cpp
+++ BOJ/#14002_가장긴증가하는부분수열4_230929.cpp
@@ -15,25 +15,24 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
 		dp.push_back(1);
-		for (int j = 1; j < i; j++) {
-			if (arr[i] > arr[j] && dp[i] < dp[j]) {
-				dp[i] = dp[j];
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+				dp[i] = dp[j] + 1;
 			}
 		}
-		dp[i]++;
 	}
 	int max_len = 0;
 	for (int i = 0; i <= N; i++) {
 		if (max_len < dp[i])
 			max_len = dp[i];
 	}
+	cout << max_len << "\n";
 	for (int i = N; i > 0; i--) {
 		if (dp[i] == max_len) {
 			st.push_back(arr[i]);
 			max_len--;
 		}
 	}
-	cout << st.size() << "\n";
 	for (int i = st.size() - 1; i >= 0; i--) {
 		cout << st[i] << " ";
 	}
