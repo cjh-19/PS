27ffdf8f06620440ad8b6a21e9d1706dd3a15585
cjh-19 <gns8812@naver.com>
2023-10-02 23:52:07 +0900

    BOJ

--- BOJ/#1912_연속합_230930.cpp
+++ BOJ/#1912_연속합_230930.cpp
@@ -1,21 +1,40 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int N;
+	cin >> N;
+	vector<int> arr(N + 1);
+	vector<int> dp;
+	vector<int> st;
 
-	int T;
-	cin >> T;
-
-	for (int i = 0; i < T; i++) {
-		int N, M, sum=1, z=1;
-		cin >> N >> M;
-		for (int j = M-N+1; j <= M; j++) {
-			sum *= j;
-			sum /= z; z++;
+	dp.push_back(0);
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+		dp.push_back(1);
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+				dp[i] = dp[j] + 1;
+			}
 		}
-		cout << sum << endl;
+	}
+	int max_len = 0;
+	for (int i = 0; i <= N; i++) {
+		if (max_len < dp[i])
+			max_len = dp[i];
+	}
+	cout << max_len << "\n";
+	for (int i = N; i > 0; i--) {
+		if (dp[i] == max_len) {
+			st.push_back(arr[i]);
+			max_len--;
+		}
+	}
+	for (int i = st.size() - 1; i >= 0; i--) {
+		cout << st[i] << " ";
 	}
 
 	return 0;
