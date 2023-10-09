95e10891f435f732087f14d07b85c365fa6919c0
cjh-19 <gns8812@naver.com>
2023-10-09 23:56:51 +0900

    BOJ

---
+++ BOJ/#24267_알고리즘수업-알고리즘의수행_231009.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	unsigned long long n;
+	cin >> n;
+	cout << n * (n - 1) * (n - 2) / 6 << "\n" << 3;
+	return 0;
+}
