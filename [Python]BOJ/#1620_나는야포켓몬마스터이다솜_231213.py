4d61c1239c69413c5d8916656c0fb7c296d6bab4
cjh-19 <gns8812@naver.com>
2023-12-14 23:43:08 +0900

    BOJ

--- [Python]BOJ/#1620_나는야포켓몬마스터이다솜_231213.py
+++ [Python]BOJ/#1620_나는야포켓몬마스터이다솜_231213.py
@@ -1,4 +1,8 @@
-num1 = int(input())
-num2 = int(input())
+import sys
 
-list = map(int, input().split())
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dict = {}
+# dict에 key-value 값으로 저장하는 방법을 시도
