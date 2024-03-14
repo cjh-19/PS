8189350b0536c1226eabf68e2ba7c85bd1c4e5be
cjh-19 <gns8812@naver.com>
2024-03-15 02:21:45 +0900

    BOJ

---
+++ [C++]BOJ/#1476_날짜계산_240313.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int e, s, m;
+    cin >> e >> s >> m;
+
+    int num = 1;
+
+    while (1) {
+
+        if ((num - e) % 15 == 0 && (num - s) % 28 == 0 && (num - m) % 19 == 0) {
+            break;
+        }
+        else num++;
+
+    }
+
+
+    cout << num << "\n";
+
+    return 0;
+    }
+}
