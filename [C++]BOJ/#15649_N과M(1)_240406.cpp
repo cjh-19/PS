7ad42da2dfd7981302567cea3efb8e565c5eb2b9
cjh-19 <gns8812@naver.com>
2024-04-06 23:07:15 +0900

    BOJ

---
+++ [C++]BOJ/#15649_N과M(1)_240406.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int choose[10], N, M;
+bool used[10];
+
+void dfs(int cnt) {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N >> M;
+	dfs(0);
+
+	return 0;
+}
