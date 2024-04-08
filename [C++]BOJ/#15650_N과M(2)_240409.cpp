e676586d76040abaf5e4902a32f84a4497e0a24b
cjh-19 <gns8812@naver.com>
2024-04-09 00:39:08 +0900

    BOJ

--- [C++]BOJ/#15650_N과M(2)_240409.cpp
+++ [C++]BOJ/#15650_N과M(2)_240409.cpp
@@ -3,10 +3,32 @@
 using namespace std;
 
 int choose[10], N, M;
-
+bool visited[10];
 
 void dfs(int cnt) {
+	if (cnt == M) {
+		int check = 0;
+		for (int i = 0; i < M - 1; i++) {
+			if (choose[i] > choose[i + 1])
+				check = 1;
+		}
 
+		if (check == 0) {
+			for (int i = 0; i < M; i++) {
+				cout << choose[i] << ' ';
+			}
+			cout << '\n';
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			choose[cnt] = i;
+			dfs(cnt + 1);
+			visited[i] = false;
+		}
+	}
 }
 
 int main() {
@@ -19,3 +41,4 @@ int main() {
 }
 
 // 조합의 개수 구하기 -> dfs를 통한 백트래킹 방식
+// 오름차순 수열만 출력하도록 조건 생성 (check 변수)
