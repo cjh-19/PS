c2a5167061382b743af52cbc2a06cff07396b772
cjh-19 <gns8812@naver.com>
2024-04-09 00:33:50 +0900

    BOJ

---
+++ [C++]BOJ/#15650_N과M(2)_240409.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int choose[10], N, M;
+
+
+void dfs(int cnt) {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> N >> M;
+	dfs(0);
+
+	return 0;
+}
+
+// 조합의 개수 구하기 -> dfs를 통한 백트래킹 방식
