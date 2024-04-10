e0fd17195dea9bdb8bd6b3190928fcd83ab2d9db
cjh-19 <gns8812@naver.com>
2024-04-10 21:24:18 +0900

    BOJ

---
+++ [C++]BOJ/#15651_N과M(3)_240410.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int choose[10], N, M;
+bool visited[10];
+
+void dfs(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << choose[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			choose[cnt] = i;
+			dfs(cnt + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> N >> M;
+	dfs(0);
+
+	return 0;
+}
