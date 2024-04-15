b2e5d70c3de1453390ae443c0700e7cb0a605a86
cjh-19 <gns8812@naver.com>
2024-04-16 00:05:04 +0900

    BOJ

--- [C++]BOJ/#14916_거스름돈_240415.cpp
+++ [C++]BOJ/#14916_거스름돈_240415.cpp
@@ -9,16 +9,19 @@ int main() {
 	int n;
 	cin >> n;
 
-	// 먼저 5원 사용
+	// n원에서 사용 가능한 최대 5원 개수
 	int start = n / 5;
-	int remain = (n - start * 5) % 2; // 5원을 두개로 뺐을 때, 짝수인지 홀수인지
-	while (remain != 0) { // 2원으로 나눴을 때 0이 아니면, 2원으로 거스리기 x
-		if (start == 0) { // 5원 이하의 숫자
-			cout << -1;
+	int remain = (n - start * 5) % 2; // 5원을 최대한 뺐을 때, 나머지 돈을 2원으로 나눌 수 있는지 확인
+	while (remain != 0) { // 2원으로 거스를 수 없는 경우
+		if (start == 0) { // 5원을 한개도 사용하지 않은 경우
+			cout << -1; // 거스를 수 없는 경우 이므로 -1 출력
 			return 0;
 		}
-
+		start -= 1; // 5원을 한개 줄임
+		remain = (n - start * 5) % 2;
+		// 5원을 최대한 사용할 수 있을 때까지 위의 과정 반복
 	}
+	cout << start + (n - start * 5) / 2;
 
 	return 0;
 }
