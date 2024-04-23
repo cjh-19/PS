3523ec0d99c82b1d5b31f848cffbc68742f23a6e
cjh-19 <gns8812@naver.com>
2024-04-24 02:58:21 +0900

    BOJ

--- [C++]BOJ/#2407_조합_240423.cpp
+++ [C++]BOJ/#2407_조합_240423.cpp
@@ -1,19 +1,43 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
 
 using namespace std;
 
-string combi(int n, int m) {
+int n, m;
+string arr[101][101];
 
+string add(string num1, string num2) {
+	int sum;
+	string result;
+
+	// 1의 자리부터 더하기
+
+	return result;
 }
 
-void solve() {
-	combi();
+string combi(int n, int m) {
+	if (n == m || m == 0)
+		return "1";
+	string& result = arr[n][m]; // 참조형 변수
+
+	// 이미 계산했으면 바로 return
+	// 메모이제이션 기법
+	if (result != "")
+		return result;
+
+	// 파스칼삼각형
+
+	return result;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	solve();
+	cin >> n >> m;
+	cout << combi(n, m);
 
 	return 0;
 }
+
+// nCm = n-1Cm-1 + n-1Cm
