53e744bd6d0715d50c88772e90772eec836d8383
cjh-19 <gns8812@naver.com>
2024-04-23 23:59:58 +0900

    BOJ

---
+++ [C++]BOJ/#2407_조합_240423.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+string combi(int n, int m) {
+
+}
+
+void solve() {
+	combi();
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
