cca172dec33cc08d3f8f6f3cb53ff92b5269d34e
cjh-19 <gns8812@naver.com>
2024-04-25 23:37:10 +0900

    BOJ

--- [C++]BOJ/#2407_조합_240423.cpp
+++ [C++]BOJ/#2407_조합_240423.cpp
@@ -12,6 +12,23 @@ string add(string num1, string num2) {
 	string result;
 
 	// 1의 자리부터 더하기
+	while (!num1.empty() || !num2.empty() || sum) {
+		if (!num1.empty())
+		{
+			sum += num1.back() - '0';
+			num1.pop_back();
+		}
+		if (!num2.empty())
+		{
+			sum += num2.back() - '0';
+			num2.pop_back();
+		}
+		result.push_back((sum % 10) + '0');
+		sum /= 10;
+	}
+
+	// 1의 자리부터 push 했으므로 뒤집긱
+	reverse(result.begin(), result.end());
 
 	return result;
 }
