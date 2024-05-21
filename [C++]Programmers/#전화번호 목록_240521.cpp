21c7dac20d5b76c4c87ef8392f87658ddeddbd45
cjh-19 <gns8812@naver.com>
2024-05-21 21:26:24 +0900

    Programmers

--- [C++]Programmers/#전화번호 목록_240521.cpp
+++ [C++]Programmers/#전화번호 목록_240521.cpp
@@ -8,6 +8,10 @@ using namespace std;
 
 bool solution(vector<string> phone_book) {
     sort(phone_book.begin(), phone_book.end());
+
+    // i번째와 i+2번째를 비교할 필요가 없음
+    // i번재 문자열이 i+2번째 문자열의 접두사라면 i+1번째는 i+2번째의 접두사기 때문임
+    // substr(n,k): 문자열을 n번 인덱스부터 k개만큼 자름
     for (int i = 0; i < phone_book.size() - 1; i++) {
         if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].length()))
             return false;
