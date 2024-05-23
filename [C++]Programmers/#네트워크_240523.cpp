cbb4287f4a1b302301e884a60299eca914d8c8d0
cjh-19 <gns8812@naver.com>
2024-05-24 01:17:47 +0900

    Programmers

--- [C++]Programmers/#네트워크_240523.cpp
+++ [C++]Programmers/#네트워크_240523.cpp
@@ -13,40 +13,32 @@ using namespace std;
 // -> 1의 영역 개수(상하좌우 연결) : 네트워크 개수
 
 int netSize = 0;
-vector<vector<bool>> visited;
-
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
+vector<bool> visited;
 
 // 영역 개수 추가 dfs
-void dfs(int x, int y, int n, vector<vector<int>> computers) {
-    visited[x][y] = true;
+void dfs(int x, int n, vector<vector<int>> computers) {
+    visited[x] = true;
 
     // 상하좌우 체크
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    for (int i = 0; i < n; i++) {
 
-        // 영역확장 가능한지 확인
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && computers[nx][ny] && visited[nx][ny] == false)
-            dfs(nx, ny, n, computers);
+        if (computers[x][i] == 1 && visited[i] == false) {
+            dfs(i, n, computers);
+        }
     }
-    return;
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
 
-    visited.resize(n, vector<bool>(n, false)); // n*n과 false로 초기화
+    visited.resize(n, false); // n, false로 초기화
 
-    // computers[x][y]의 값이 1이고 방문안했는지 확인
+    // 방문안했는지 확인
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (computers[i][j] == 1 && visited[i][j] == false) {
-                // dfs로 영역 확장(visited 체크)
-                dfs(i, j, n, computers);
-                netSize++;
-            }
+        if (visited[i] == false) {
+            // dfs로 영역 확장(visited 체크)
+            dfs(i, n, computers);
+            netSize++;
         }
     }
 
