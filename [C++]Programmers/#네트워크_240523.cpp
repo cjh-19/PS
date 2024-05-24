2e83d736f61797486bd805b6ddbb811d48a1f7ae
cjh-19 <gns8812@naver.com>
2024-05-24 12:59:04 +0900

    Programmers

--- [C++]Programmers/#네트워크_240523.cpp
+++ [C++]Programmers/#네트워크_240523.cpp
@@ -2,26 +2,18 @@
 #include <vector>
 
 using namespace std;
-// 1 1 0
-// 1 1 0
-// 0 0 1
-
-// 1 1 0
-// 1 1 1
-// 0 1 1
-
-// -> 1의 영역 개수(상하좌우 연결) : 네트워크 개수
 
 int netSize = 0;
 vector<bool> visited;
 
-// 영역 개수 추가 dfs
+// dfs -> 방문 체크(네트워크에 포함해주는 것)
+// dfs를 실행시킨 컴퓨터의 네트워크에 들어가는 컴퓨터를 찾는 것
 void dfs(int x, int n, vector<vector<int>> computers) {
-    visited[x] = true;
+    visited[x] = true; // 방문 체크
 
-    // 상하좌우 체크
+    // x번 computers와 i번 컴퓨터가 연결됐는지, 아직 방문하지 않은 컴퓨터인지 체크하고 (n개의 컴퓨터 모두 체크)
+    // 그렇다면 dfs 실행해서 방문표시
     for (int i = 0; i < n; i++) {
-
         if (computers[x][i] == 1 && visited[i] == false) {
             dfs(i, n, computers);
         }
@@ -35,8 +27,8 @@ int solution(int n, vector<vector<int>> computers) {
 
     // 방문안했는지 확인
     for (int i = 0; i < n; i++) {
-        if (visited[i] == false) {
-            // dfs로 영역 확장(visited 체크)
+        if (visited[i] == false) { // 방문 안했다면 (네트워크에 포함되지 않은 컴퓨터)
+            // dfs 실행 횟수: 네트워크 개수 (visited 체크)
             dfs(i, n, computers);
             netSize++;
         }
