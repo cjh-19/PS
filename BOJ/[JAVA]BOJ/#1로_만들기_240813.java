4736b64ed73d7d76aaf9d8837f1a428f61dcebef
cjh-19 <gns8812@naver.com>
2024-08-13 01:04:34 +0900

    BOJ
    refactoring

--- BOJ/[JAVA]BOJ/#1로_만들기_240813.java
+++ BOJ/[JAVA]BOJ/#1로_만들기_240813.java
@@ -1,3 +1,5 @@
+/* 2차 풀이 */
+
 import java.util.*;
 import java.io.*;
 
@@ -12,35 +14,67 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         dp = new int[n + 1];
 
-        // 초기값 설정
-        dp[0] = 0;
-        dp[1] = 0;
-
-        System.out.println(recurDp(n));
-        br.close();
-    }
-
-    // 재귀를 통해 dp를 채워 나갈 함수
-    static int recurDp(int N){
-        if(N > 1 && dp[N] == 0) { // N이 1보다 크고, dp 값 저장이 안됐다면
-            // 6으로 나눠지는 경우 - 3과 2와 1빼는 경우의 재귀함수를 모두 호출해서 최솟값을 dp[N]에 저장한다
-            if (N % 6 == 0) {
-                dp[N] = Math.min(Math.min(recurDp(N / 3), recurDp(N / 2)), recurDp(N - 1)) + 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + 1;
+            if(i % 3 == 0){
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
             }
-            // 3으로 나눠지는 경우
-            else if (N % 3 == 0) {
-                dp[N] = Math.min(recurDp(N - 1), recurDp(N / 3)) + 1;
-            }
-            // 2로 나눠지는 경우
-            else if (N % 2 == 0) {
-                dp[N] = Math.min(recurDp(N - 1), recurDp(N / 2)) + 1;
-            }
-            // 6으로 나눠지지 않는 경우(1빼기)
-            else {
-                dp[N] = recurDp(N - 1) + 1;
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
             }
         }
 
-        return dp[N];
+        System.out.println(dp[n]);
+        br.close();
     }
 }
+
+
+/* 1차 풀이 */
+
+// import java.util.*;
+// import java.io.*;
+
+// public class Main {
+//     static int n; // 입력 받을 n
+//     static int[] dp;
+
+//     public static void main(String[] args) throws IOException {
+//         // 정수 n 입력
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//         n = Integer.parseInt(br.readLine());
+//         dp = new int[n + 1];
+
+//         // 초기값 설정
+//         dp[0] = 0;
+//         dp[1] = 0;
+
+//         System.out.println(recurDp(n));
+//         br.close();
+//     }
+
+//     // 재귀를 통해 dp를 채워 나갈 함수
+//     static int recurDp(int N){
+//         if(N > 1 && dp[N] == 0) { // N이 1보다 크고, dp 값 저장이 안됐다면
+//             // 6으로 나눠지는 경우 - 3과 2와 1빼는 경우의 재귀함수를 모두 호출해서 최솟값을 dp[N]에 저장한다
+//             if (N % 6 == 0) {
+//                 dp[N] = Math.min(Math.min(recurDp(N / 3), recurDp(N / 2)), recurDp(N - 1)) + 1;
+//             }
+//             // 3으로 나눠지는 경우
+//             else if (N % 3 == 0) {
+//                 dp[N] = Math.min(recurDp(N - 1), recurDp(N / 3)) + 1;
+//             }
+//             // 2로 나눠지는 경우
+//             else if (N % 2 == 0) {
+//                 dp[N] = Math.min(recurDp(N - 1), recurDp(N / 2)) + 1;
+//             }
+//             // 6으로 나눠지지 않는 경우(1빼기)
+//             else {
+//                 dp[N] = recurDp(N - 1) + 1;
+//             }
+//         }
+
+//         return dp[N];
+//     }
+// }
