699d4568785e2a19762d9792fd718ae9fd17b591
cjh-19 <gns8812@naver.com>
2024-08-21 00:13:10 +0900

    Programmers
    java

--- BOJ/[JAVA]BOJ/BOJ_RGB거리.java
+++ BOJ/[JAVA]BOJ/BOJ_RGB거리.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     static int N; // 집의 수
-    static int[] R, G, B; // 각 집의 빨 초 파 비용
+    static int[][] RGB; // 각 집의 빨 초 파 비용
     static int[][] dp;
 
     public static void main(String[] args) throws IOException  {
@@ -24,41 +24,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        R = new int[N];
-        G = new int[N];
-        B = new int[N];
+        RGB = new int[N][3];
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            R[i] = Integer.parseInt(st.nextToken());
-            G[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st.nextToken());
+            RGB[i][0] = Integer.parseInt(st.nextToken());
+            RGB[i][1] = Integer.parseInt(st.nextToken());
+            RGB[i][2] = Integer.parseInt(st.nextToken());
         }
 
         // dp 계산
-        // dp[i][j] += min(dp[i-1][1], dp[i-1][2]) ...
+        // dp[i][0] += min(dp[i-1][1], dp[i-1][2]) + RGB[i][0] ...
 
         dp = new int[N][3];
 
         // 첫번째 집 각 색 비용
-        dp[0][0] = R[0];
-        dp[0][1] = G[0];
-        dp[0][2] = B[0];
+        dp[0][0] = RGB[0][0];
+        dp[0][1] = RGB[0][1];
+        dp[0][2] = RGB[0][2];
 
         // 1번 집부터 N-1번 집까지
         for(int i=1; i<N; i++){
-            for(int j=0; j<3; j++){
-                if(j == 0){ // R 색상 선택
-                    dp[i][j] += Math.min(dp[i-1][1], dp[i-1][2]) + R[i];
-                }
-                else if(j == 1){ // G 색상 선택
-                    dp[i][j] += Math.min(dp[i-1][0], dp[i-1][2]) + G[i];
-                }
-                else{ // B 색상 선택
-                    dp[i][j] += Math.min(dp[i-1][0], dp[i-1][1]) + B[i];
-                }
-            }
+            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]) + RGB[i][0];
+            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]) + RGB[i][1];
+            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]) + RGB[i][2];
         }
+
         System.out.println(Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]));
         br.close();
     }
