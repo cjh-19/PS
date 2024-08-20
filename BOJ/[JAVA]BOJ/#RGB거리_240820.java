5294040d62fee79ea5827a1b6a49ab303c5d2ad3
cjh-19 <gns8812@naver.com>
2024-08-21 01:22:59 +0900

    Programmers
    java

--- BOJ/[JAVA]BOJ/#RGB거리_240820.java
+++ BOJ/[JAVA]BOJ/#RGB거리_240820.java
@@ -45,9 +45,9 @@ public class Main {
 
         // 1번 집부터 N-1번 집까지
         for(int i=1; i<N; i++){
-            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]) + RGB[i][0];
-            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]) + RGB[i][1];
-            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]) + RGB[i][2];
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + RGB[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + RGB[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + RGB[i][2];
         }
 
         System.out.println(Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]));
