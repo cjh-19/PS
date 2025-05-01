cae77dd32396cf674de11d04835b0fb314560a1d
cjh-19 <gns8812@naver.com>
2025-05-02 02:47:08 +0900

    BOJ

---
+++ BOJ/[C++]BOJ/#11866_요세푸스_문제_0_250502.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N, K;
+	cin >> N >> K;
+	queue<int> q;
+
+	for (int i = 1; i <= N; i++) {
+		q.push(i);
+	}
+
+	int cnt = 0;
+	cout << "<";
+	while (!q.empty()) {
+		cnt++;
+		int n = q.front();
+		q.pop();
+		if (cnt == K) {
+			if (q.empty()) cout << n << ">";
+			else cout << n << ", ";
+			cnt = 0;
+		}
+		else q.push(n);
+	}
+
+	return 0;
+}
