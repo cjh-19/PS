ba4ecd4498f675c77b5c3f615c61189e1faf39e0
cjh-19 <gns8812@naver.com>
2025-05-02 02:46:22 +0900

    BOJ

---
+++ BOJ/[C++]BOJ/#30802_웰컴_키트_250502.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	long long N;
+	cin >> N;
+	long long t[6];
+	long long T, P;
+	long long mint = 0;
+	for (int i = 0; i < 6; i++) {
+		cin >> t[i];
+	}
+	cin >> T >> P;
+
+	for (int i = 0; i < 6; i++) {
+		mint += (t[i] + T - 1) / T;
+	}
+
+	cout << mint << "\n" << N / P << " " << N % P;
+
+	return 0;
+}
