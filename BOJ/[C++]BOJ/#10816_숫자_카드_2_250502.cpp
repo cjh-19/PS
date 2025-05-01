218119418bbd85c7f2901871516f47f57c4c7d2f
cjh-19 <gns8812@naver.com>
2025-05-02 02:47:02 +0900

    BOJ

---
+++ BOJ/[C++]BOJ/#10816_숫자_카드_2_250502.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int N, M;
+unordered_map<int, int> card;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+		card[n]++;
+	}
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int n;
+		cin >> n;
+		cout << card[n] << " ";
+	}
+
+	return 0;
+}
