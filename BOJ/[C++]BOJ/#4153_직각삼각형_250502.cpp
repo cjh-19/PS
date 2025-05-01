6a8eec9698b3823d40bec686e9715497be977336
cjh-19 <gns8812@naver.com>
2025-05-02 02:46:17 +0900

    BOJ

---
+++ BOJ/[C++]BOJ/#4153_직각삼각형_250502.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	while (1) {
+		vector<int> t(3);
+		cin >> t[0] >> t[1] >> t[2];
+		if (t[0] == 0 && t[1] == 0 && t[2] == 0) break;
+
+		sort(t.begin(), t.end());
+		if (t[0] * t[0] + t[1] * t[1] == t[2] * t[2]) cout << "right" << "\n";
+		else cout << "wrong" << "\n";
+	}
+
+	return 0;
+}
