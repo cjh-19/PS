6b0c7d33d7b1ada73f267d38d78b41e189f18012
cjh-19 <gns8812@naver.com>
2025-05-02 00:54:42 +0900

    BOJ

---
+++ BOJ/[C++]BOJ/#1629_곱셈_250502.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll A, B, C;
+
+ll multiple(ll B) {
+	if (B == 0) return 1;
+
+	ll half = multiple(B / 2);
+	ll result = half * half % C;
+
+	// 홀수인 경우
+	if (B % 2 == 1) result = result * A % C;
+	return result;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> A >> B >> C;
+	cout << multiple(B);
+
+	return 0;
+}
