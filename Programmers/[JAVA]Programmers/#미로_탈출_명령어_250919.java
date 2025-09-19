d341da65d2efdab0dcc0e62ed8ae038b92a37f3a
cjh-19 <gns8812@naver.com>
2025-09-19 23:59:30 +0900

    Programmers

---
+++ Programmers/[JAVA]Programmers/#미로_탈출_명령어_250919.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    private static String answer;
+    private static int[] dx = {1, 0, 0, -1};
+    private static int[] dy = {0, -1, 1, 0};
+    private static String[] dd = {"d", "l", "r", "u"};
+    private static int N, M;
+    private static class Node {
+        int x;
+        int y;
+        int dist;
+        StringBuilder sb;
+        public Node(int x, int y, int dist, StringBuilder sb) {
+            this.x=x;
+            this.y=y;
+            this.dist=dist;
+            this.sb=sb;
+        }
+    }
+    
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        this.N=n; this.M=m;
+        
+        answer = "impossible";
+        int minDist = Math.abs(x-r) + Math.abs(y-c);
+        if(minDist > k || (k-minDist)%2==1) return answer;
+        bfs(x, y, r, c, k);
+        
+        return answer;
+    }
+    
+    private static void bfs(int x, int y, int r, int c, int k) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(x,y, 0, new StringBuilder()));
+        
+        while(!q.isEmpty()) {
+            Node info = q.poll();
+            int xpos = info.x;
+            int ypos = info.y;
+            int dist = info.dist;
+            
+            if(xpos == r && ypos == c) {
+                if(k==dist) {
+                    answer = info.sb.toString();
+                    return;
+                }
+            }
+            if(k<=dist) continue;
+            
+            for(int d=0; d<4; d++) {
+                int nx = xpos + dx[d];
+                int ny = ypos + dy[d];
+                
+                if(!valid(nx, ny)) continue;
+                
+                int rest = k - (dist + 1);
+                int mind = Math.abs(nx - r) + Math.abs(ny - c);
+                if (mind > rest || ((rest - mind) & 1) == 1) continue;
+                
+                StringBuilder nsb = new StringBuilder();
+                nsb.append(info.sb.toString());
+                nsb.append(dd[d]);
+                
+                q.offer(new Node(nx, ny, dist+1, nsb));
+            }
+        }
+    }
+    
+    private static boolean valid(int x, int y) {
+        return x>=1 && x<=N && y>=1 && y<=M;
+    }
+}
